c14b46a3a68572361c09573865fc601a33a2d846
tukks3 <salmaabdullah2000@gmail.com>
2026-01-16 16:14:51 +0000

    typo

--- client/src/components/AllOppsGrid.jsx
+++ client/src/components/AllOppsGrid.jsx
@@ -68,7 +68,7 @@ function AllOppsGrid({ result, setResult, error, setError }) {
             <div className="grid-champ_container">
                 {icons.map((url) => (
                     <button key={url} className="grid-champ_icon" onClick={() => handleClick(url)}>
-                        <img src={apiurl(url)} loading="lazy" width={48} height={48} alt="opp icon" />
+                        <img src={apiUrl(url)} loading="lazy" width={48} height={48} alt="opp icon" />
                     </button>
                 ))}
             </div>
