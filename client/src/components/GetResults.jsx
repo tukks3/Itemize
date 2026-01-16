a49836b76c571cbda44f8d519a28c50bcb23e7a7
tukks3 <salmaabdullah2000@gmail.com>
2026-01-16 16:45:49 +0000

    h

--- client/src/components/GetResults.jsx
+++ client/src/components/GetResults.jsx
@@ -2,7 +2,7 @@ import{ apiUrl } from "../components/Api";
 
 async function GetResults(champId, oppTeam) {
 
-    const res = await fetch(apiUrl("/api/getResults", {
+    const res = await fetch(apiUrl("/api/getResults"), {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -11,7 +11,7 @@ async function GetResults(champId, oppTeam) {
             champId,
             oppTeam,
         }),
-    }));
+    });
     const data = await res.json();
     return data;
 }
