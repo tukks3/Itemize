5385eff44ba0875266e58ff52306e4cb85e3e36b
tukks3 <salmaabdullah2000@gmail.com>
2026-01-16 17:54:10 +0000

    dfg

--- server/test.py
+++ server/test.py
@@ -4,9 +4,9 @@ from src.get_profiles import ItemProfile, ChampProfile, CounterProfiles
 from src.get_icons import get_champ_icons, get_item_icons
 
 ids = GetIds()
-ChampUsage(ids) # Creates 'champ_items.csv' for items used by each champ
+#ChampUsage(ids) # Creates 'champ_items.csv' for items used by each champ
 ItemProfile(ids) # Creates 'item_profiles.csv' for stats of each item
-ChampProfile(ids) # Creates 'champ_profiles.csv' for info on all champs
-CounterProfiles() # Creates 'opp_counters.csv' for opponent weaknesses
-get_champ_icons()
-get_item_icons()
+#ChampProfile(ids) # Creates 'champ_profiles.csv' for info on all champs
+#CounterProfiles() # Creates 'opp_counters.csv' for opponent weaknesses
+#get_champ_icons()
+#get_item_icons()
